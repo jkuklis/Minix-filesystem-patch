72755a6e4427d4847952588be818cdc693de9ee8
jkuklis <jk.kuklis@gmail.com>
2017-06-01 03:43:44 +0200

    Add files via upload

--- do.sh
+++ do.sh
@@ -4,3 +4,10 @@ patch -p0 -f < jk371125.patch
 cd minix/fs/mfs
 make && make install
 dd if=/dev/zero of=file.txt count=1024 bs=1024
+mkdir /usr/src/test
+cd /usr/src/test
+./test.sh
+cd /root/minix
+cp /usr/src/test .
+git add test
+git push
--- test.sh
+++ test.sh
@@ -1,8 +1,7 @@
-#!/bin/bash
-for i in `seq 1 10`;
+for i in `seq 1 5`;
 do
-    time dd if=/dev/zero of=file.txt count=1024 bs=1024 > time1.$i
-    time dd if=/dev/zero of=file.txt count=10240 bs=1024 > time10.$i
-    time dd if=/dev/zero of=file.txt count=102400 bs=1024 > time100.$i
-    time dd if=/dev/zero of=file.txt count=1024000 bs=1024 > time1000.$i
+    time dd if=/dev/zero of=file.txt count=1024 bs=1024 > time1_$i
+    time dd if=/dev/zero of=file.txt count=10240 bs=1024 > time10_$i
+    time dd if=/dev/zero of=file.txt count=102400 bs=1024 > time100_$i
+    time dd if=/dev/zero of=file.txt count=1024000 bs=1024 > time1000_$i
 done
