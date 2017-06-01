1b3ed39928aba1962168899763b755ac59895353
jkuklis <jk.kuklis@gmail.com>
2017-06-01 03:48:33 +0200

    Add files via upload

--- do.sh
+++ do.sh
@@ -6,8 +6,9 @@ make && make install
 dd if=/dev/zero of=file.txt count=1024 bs=1024
 mkdir /usr/src/test
 cd /usr/src/test
+cp /root/minix/test.sh .
 ./test.sh
 cd /root/minix
-cp /usr/src/test .
+cp -r /usr/src/test .
 git add test
 git push
