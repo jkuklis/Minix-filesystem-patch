8af787cf975c1b29db0ebf365b462cafbd9b8dce
jkuklis <jk.kuklis@gmail.com>
2017-06-01 04:05:41 +0200

    Add files via upload

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 for i in `seq 1 5`;
 do
-    time dd if=/dev/zero of=file.txt count=1024 bs=1024 > time1_$i
-    time dd if=/dev/zero of=file.txt count=10240 bs=1024 > time10_$i
-    time dd if=/dev/zero of=file.txt count=102400 bs=1024 > time100_$i
-    time dd if=/dev/zero of=file.txt count=1024000 bs=1024 > time1000_$i
+    time dd if=/dev/zero of=file.txt count=1024 bs=1024 2> time1_$i
+    time dd if=/dev/zero of=file.txt count=10240 bs=1024 2> time10_$i
+    time dd if=/dev/zero of=file.txt count=102400 bs=1024 2> time100_$i
+    time dd if=/dev/zero of=file.txt count=1024000 bs=1024 2> time1000_$i
 done
